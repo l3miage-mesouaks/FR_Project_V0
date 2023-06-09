303d85911945eecdd1b7a1fc1d8bd347a3e06dba
l3miage-mesouaks <salaheddin.mesouak@etu.univ-grenoble-alpes.fr>
2023-06-09 16:04:19 +0200

    Version_0

---
+++ FR_project/FR_project/Models/Entreprise.cs
@@ -0,0 +1,10 @@
+﻿namespace FR_project.Models
+{
+    public class Entreprise
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Adresse { get; set; }
+        public string Mail { get; set;}
+    }
+}
---
+++ FR_project/FR_project/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace FR_project.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Nom_produit { get; set; }
+
+        public decimal Price { get; set; }
+
+    }
+}
